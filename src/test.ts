747ecd7113108396380360773cc089470240bb4a
vitalii-minchuk <mcmin4doutsch@gmail.com>
2023-02-05 11:24:13 +0200

    test ts

---
+++ src/test.ts
@@ -0,0 +1,11 @@
+interface Data {
+  group: number,
+  name: string
+}
+
+const data: Data[] = [
+  {group: 1, name: "name 1"},
+  {group: 2, name: "name 2"},
+  {group: 1, name: "name 3"},
+  {group: 1, name: "name 2"},
+]
